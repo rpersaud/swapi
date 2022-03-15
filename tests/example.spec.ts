14b1b85c2a32f94b9d7a69c4be3cd006c0bdd3b9
Robert Persaud <robert.persaud@coxautoinc.com>
2022-03-15 04:05:30 -0400

    adding ui automation testing

---
+++ tests/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
+
+test('swapi test', async({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    await expect(page).toHaveTitle(/whiteSpace/);
+
+    await page.pause();
+});
