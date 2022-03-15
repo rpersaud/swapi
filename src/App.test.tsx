665f56ce4888dfa415a8eb34327ddec66a750ab3
Robert Persaud <robert.persaud@coxautoinc.com>
2022-03-15 03:48:07 -0400

    updated unit coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act} from '@testing-library/react';
 import App from './App';
+import axios, { AxiosResponse } from 'axios';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// mock data
+const mockData = {
+  data: {
+    results: [
+      {
+        name: 'Mock Vader',
+        films: [
+          "https://swapi.dev/api/films/1/",
+          "https://swapi.dev/api/films/2/",
+          "https://swapi.dev/api/films/3/"
+       ]
+      }
+    ]
+  }
+};
+
+describe('App', () => {
+  let wrapper;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+  test('should render with loading', () => {
+    render(<App />);
+    const loadingElement = screen.getByText(/...loading/i);
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  test('should load data with initial api call', async () => {
+    const mAxiosResponse = mockData as AxiosResponse;
+    jest.spyOn(axios, 'get').mockResolvedValueOnce(mAxiosResponse);
+    render(<App />);
+    expect(screen.getByText('...loading')).toBeInTheDocument();
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(await screen.findByText('Mocks Vader')).toBeInTheDocument();
+  });
 });
